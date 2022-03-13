a2920e7a854ef4c12e2ae2e3cf51e0647b5b268d
farihulrouf <farihulrouf@gmail.com>
2022-03-13 22:28:43 +0700

    add price fix

--- pages/price.js
+++ pages/price.js
@@ -6,8 +6,8 @@ const Price = () => {
              <div className="max-w-screen-xl mx-auto lg:pl-20 lg:pr-20  pl-4 pr-4 pt-4 pb-4 font-palo pt-36">
                  <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-4 gap-y-4 sm:gap-y-36">
                      {product.map((item, index) => (
-                         <div className="col-span-1">
-                             <CardProduct key={index} item={item} />
+                         <div key={index} className="col-span-1">
+                             <CardProduct  item={item} />
                          </div>
                      ))}
                  </div>
