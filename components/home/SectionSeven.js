19a31d739c35629e5b995156e0d9d510916583fc
farihulrouf <farihulrouf@gmail.com>
2022-03-13 20:54:19 +0700

    responsive 2.1

--- components/home/SectionSeven.js
+++ components/home/SectionSeven.js
@@ -2,12 +2,12 @@
 const SectionSeven = () => {
     return (
         <div className="bg-black">
-             <div className="max-w-screen-xl mx-auto p-4 mt-16 mb-8">
+             <div className="max-w-screen-xl mx-auto p-4 mt-16 mb-8 font-palo">
                 {/* <img className="w-36 absolute top-0" src="/img/bg_section_seven.svg" /> */}
                  <div className="grid grid-cols-12">
                     <div className="md:col-span-6 col-span-12">
                          <h5 className="text-teal-300 text-xl py-2">Become a member</h5>
-                         <h2 className="text-white text-5xl py-2">Ready to try different work environment now?</h2> 
+                         <h2 className="text-white text-4xl md:text-5xl py-2 font-semibold">Ready to try different work environment now?</h2> 
                          <p className="text-white max-w-lg py-8 text-base">With its unique concept, 
                          Cosmohub wants to promote productivity, entrepreneurship and business culture by creating a new kind of space, 
                          where people can work, meet, learn or just hang out. So,  
