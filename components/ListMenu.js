508cf7439d704afe0f9a92c624b1bfaecb5ff968
farihulrouf <farihulrouf@gmail.com>
2022-03-16 10:31:13 +0700

    fix nav1.0

--- components/ListMenu.js
+++ components/ListMenu.js
@@ -4,7 +4,7 @@ import {listMenu} from '../components/data/dataContent'
 const ListMenu = () => {
     return(
         <React.Fragment>
-            <ul className="sm:relative bg-gradient-to-r from-blue-500 sm:mr-8 mr-1 sm:flex sm:justify-end sm:py-0 sm:top-0 sm:space-x-5 absolute py-2 top-[60px] left-0 right-0">
+            <ul className="sm:relative sm:mr-8 mr-1 sm:flex sm:justify-end sm:py-0 sm:top-0 sm:space-x-5 absolute py-2 top-[60px] left-0 right-0">
                 {listMenu.map((item,index) =>
                     <li key={index} className="text-sm text-white sm:pl-0 sm:pb-0 pl-8 pb-1"><Link href={item.link}><a>{item.text}</a></Link></li>
                 )}
