68c7925e3b14b0c0315b8f7d7f791936e80f621d
farihulrouf <farihulrouf@gmail.com>
2022-03-16 11:21:36 +0700

    fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer className="bg-black bg-cover bg-bottom bg-[url('/img/footer_bg.png')]">
              <div className="max-w-screen-xl mx-auto p-4">
                 <div className="pt-16 grid grid-cols-12 gap-4 pb-8">
-                    <div className="md:col-span-6 col-span-8">
+                    <div className="md:col-span-6 col-span-9">
                         <div className="flex space-x-1">
                              <img className="w-12 h-12" src="/img/logo.svg" alt="logo" />
                              <p className="text-white text-lg">CosmoHub</p>
@@ -16,7 +16,7 @@ const Footer = () => {
                         </div>
 
                     </div>
-                    <div className="md:col-span-3 col-span-4">
+                    <div className="md:col-span-3 col-span-3">
                         <h5 className="text-white">Menu</h5>
                         <ul className="py-1">
                         <li className="text-white py-2">Pricing</li>
